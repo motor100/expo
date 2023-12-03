a388d405f0936732149ab515d42a6c533bb4d49c
Evgeny <260443@mail.ru>
2023-12-03 17:28:27 +0500

    Callback section man

--- resources/views/callback-section.blade.php
+++ resources/views/callback-section.blade.php
@@ -15,7 +15,8 @@
         </button>
       </div>
       <div class="callback-logo-fragment">
-        <img src="/img/callback-logo-fragment.png" alt="">
+        <img src="/img/callback-logo-fragment-desktop.png" class="callback-logo-fragment-desktop" alt="">
+        <img src="/img/callback-logo-fragment-mobile.png" class="callback-logo-fragment-mobile" alt="">
       </div>
       <div class="callback-man">
         <img src="/img/callback-man.png" alt="">
